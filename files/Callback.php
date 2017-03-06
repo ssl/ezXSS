a4d3aab475ea0bbb4abc9c45d2bd81dbfa75aca2
Elye$a <hi@za.ax>
2017-03-06 22:09:26 +0100

    Update Callback.php

--- files/Callback.php
+++ files/Callback.php
@@ -11,10 +11,17 @@
   $json = file_get_contents('php://input');
   $jsonDecoded = json_decode($json, true);
 
-  //> Information about user and domain
+  //> Some information and settings
   $userIP = $_SERVER["REMOTE_ADDR"];
   $userIP2 = htmlspecialchars((isset($_SERVER["HTTP_X_FORWARDED_FOR"])) ? ", " . ($_SERVER["HTTP_X_FORWARDED_FOR"]) : "");
   $domain = htmlspecialchars($_SERVER['SERVER_NAME']);
+
+  //> How much chars to send via MAIL - a long dom can take >10min in clients like Gmail to process.
+  //> 0 is full, 500 is recommened since full dom is not always needed in email
+  $domPart = 500;
+
+  if($domPart > 0) $domExtra = "\n\nView full dom on the report page or change this setting in the callback file";
+  else $domExtra = "";
 
   //> Require Database for query and
   require_once __DIR__ . "/manage/src/Database.php";
@@ -30,7 +37,7 @@
   //> Send email
   $htmlTemplate = str_replace(
     ["{{id}}", "{{domain}}", "{{url}}", "{{ip}}", "{{referer}}", "{{user-agent}}", "{{cookies}}", "{{dom}}", "{{origin}}", "{{time}}"],
-    [$id, $domain, htmlspecialchars($jsonDecoded["uri"]), $userIP.$userIP2, htmlspecialchars($jsonDecoded["referer"]), htmlspecialchars($jsonDecoded["user-agent"]), htmlspecialchars($jsonDecoded["cookies"]), htmlspecialchars($jsonDecoded["dom"]), htmlspecialchars($jsonDecoded["origin"]), time()],
+    [$id, $domain, htmlspecialchars($jsonDecoded["uri"]), $userIP.$userIP2, htmlspecialchars($jsonDecoded["referer"]), htmlspecialchars($jsonDecoded["user-agent"]), htmlspecialchars($jsonDecoded["cookies"]), htmlspecialchars(substr($jsonDecoded["dom"], 0, $domPart)) . $domExtra, htmlspecialchars($jsonDecoded["origin"]), time()],
     file_get_contents("manage/src/templates/site/mail.htm")
   );
 
