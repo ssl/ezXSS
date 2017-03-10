5c78394d789bd0d3ec2fa615d6baf652cbf7739e
Elye$a <hi@za.ax>
2017-03-10 19:41:21 +0100

    Deleted userIP2

--- files/Callback.php
+++ files/Callback.php
@@ -13,7 +13,6 @@
 
   //> Some information and settings
   $userIP = $_SERVER["REMOTE_ADDR"];
-  $userIP2 = htmlspecialchars((isset($_SERVER["HTTP_X_FORWARDED_FOR"])) ? ", " . ($_SERVER["HTTP_X_FORWARDED_FOR"]) : "");
   $domain = htmlspecialchars($_SERVER['SERVER_NAME']);
 
   //> How much chars to send via MAIL - a long dom can take >10min in clients like Gmail to process.
@@ -29,7 +28,7 @@
 
   //> Insert into DB
   $id = $database->lastInsertId("INSERT INTO reports (`cookies`, `dom`, `origin`, `referer`, `screenshot`, `uri`, `user-agent`, `ip`, `time`) VALUES (:cookies, :dom, :origin, :referer, :screenshot, :uri, :userAgent, :ip, :time)",
-  array(":cookies" => $jsonDecoded["cookies"], ":dom" => $jsonDecoded["dom"], ":origin" => $jsonDecoded["origin"],":referer" => $jsonDecoded["referrer"], ":screenshot" => $jsonDecoded["screenshot"], ":uri" => $jsonDecoded["uri"], ":userAgent" => $jsonDecoded["user-agent"],":ip" => $userIP . $userIP2, ":time" => time()));
+  array(":cookies" => $jsonDecoded["cookies"], ":dom" => $jsonDecoded["dom"], ":origin" => $jsonDecoded["origin"],":referer" => $jsonDecoded["referrer"], ":screenshot" => $jsonDecoded["screenshot"], ":uri" => $jsonDecoded["uri"], ":userAgent" => $jsonDecoded["user-agent"],":ip" => $userIP, ":time" => time()));
 
   //> Alert email
   $email = $database->newQueryArray("SELECT * FROM settings WHERE setting='email' LIMIT 1");
@@ -37,7 +36,7 @@
   //> Send email
   $htmlTemplate = str_replace(
     ["{{id}}", "{{domain}}", "{{url}}", "{{ip}}", "{{referer}}", "{{user-agent}}", "{{cookies}}", "{{dom}}", "{{origin}}", "{{time}}"],
-    [$id, $domain, htmlspecialchars($jsonDecoded["uri"]), $userIP.$userIP2, htmlspecialchars($jsonDecoded["referrer"]), htmlspecialchars($jsonDecoded["user-agent"]), htmlspecialchars($jsonDecoded["cookies"]), htmlspecialchars(substr($jsonDecoded["dom"], 0, $domPart)) . $domExtra, htmlspecialchars($jsonDecoded["origin"]), time()],
+    [$id, $domain, htmlspecialchars($jsonDecoded["uri"]), $userIP, htmlspecialchars($jsonDecoded["referrer"]), htmlspecialchars($jsonDecoded["user-agent"]), htmlspecialchars($jsonDecoded["cookies"]), htmlspecialchars(substr($jsonDecoded["dom"], 0, $domPart)) . $domExtra, htmlspecialchars($jsonDecoded["origin"]), time()],
     file_get_contents("manage/src/templates/site/mail.htm")
   );
 
