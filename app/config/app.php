8906c1d0758e9c7711c98a4841cbb6e68aaaf9df
ssl <contact@elyesa.nl>
2023-01-03 17:30:31 +0100

    Refactored and added documentation

--- app/config/app.php
+++ app/config/app.php
@@ -1,4 +1,13 @@
 <?php
 
+// Debug modus will show and report any kind of errors, do not enable this unless you are debugging something
 define('debug', true);
+
+// Current ezXSS version. Do not edit this
 define('version', '4.0');
+
+// Defines the current host
+define('host', e($_SERVER['HTTP_HOST']));
+
+// Defines the current url
+define('url', e("//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"));
