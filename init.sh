75ffefb2b30e32440b9540c0ed4da9876d59d9ba
BoBeR182 <525433+BoBeR182@users.noreply.github.com>
2023-03-04 23:54:11 +0000

    fixes #96 #106
    fixes screenshot folder permissions to be read/write by both the app and the server

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ useradd --system --uid "$PUID" --gid "$PGID" ezxss || true
 chown ezxss: /var/www/html -R || true
 chown ezxss /var/log/apache2/error.log || true
 chown ezxss /var/log/apache2/other_vhosts_access.log || true
-chown www-data:ezxss /var/www/html/assets/img
+chmod 777 /var/www/html/assets/img || true
 echo "Launching application with UID $PUID and GID $PGID"
 runuser -u ezxss apache2-foreground
