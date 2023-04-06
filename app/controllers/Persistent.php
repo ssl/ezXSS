08ec0cfbae2fb772795f224e9c08073cefbbadbf
ssl <contact@elyesa.nl>
2023-04-06 19:26:22 +0200

    ezXSS proxy

--- app/controllers/Persistent.php
+++ app/controllers/Persistent.php
@@ -116,6 +116,7 @@ class Persistent extends Controller
 
         // Render all rows
         $this->view->renderData('time', date('F j, Y, g:i a', $session['time']));
+        $this->view->renderData('requests', 'test');
 
         $record = $this->reader->country($session['ip']);
         $this->view->renderData('country', strtolower($record->country->isoCode ?? 'xx'));
