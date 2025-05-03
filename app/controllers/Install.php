ebc5d5932f253df7134579ee01e70360172ee56d
ssl <git@elyesa.nl>
2025-05-03 13:22:05 +0200

    Update version number in install

--- app/controllers/Install.php
+++ app/controllers/Install.php
@@ -41,7 +41,7 @@ class Install extends Controller
                 }
 
                 // Create the database tables and rows
-                $sql = file_get_contents(__DIR__ . '/../sql/4.2.sql');
+                $sql = file_get_contents(__DIR__ . '/../sql/4.3.sql');
                 $database = Database::openConnection();
                 $database->exec($sql);
                 $database->exec('ALTER DATABASE `' . DB_NAME . '` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;');
